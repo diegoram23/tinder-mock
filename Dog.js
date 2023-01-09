21a5f32eacd8ebe5cee68b832e364b30e2ec77c2
diegoram23 <diego.pta.oc@gmail.com>
2023-01-08 16:38:33 -0800

    Updated the Dog constructor function

--- Dog.js
+++ Dog.js
@@ -1,10 +1,18 @@
-function Dog(data){
-    Object.assign(this, data)
+function Dog(data) {
 
-    this.getDogHtml = function(){
-        const { name, avatar, age, bio, hasBeenSwiped, hasBeenLiked } = this
-        return ``
+Object.assign(this, data)
+
+    this.getDogHtml = function () {
+        const { avatar, name, age, bio } = this
+        document.getElementById('content').innerHTML = `<div class="img-container">
+        <img class="img" src="${avatar}">
+        <h2 class="img-text-info">${name}, ${age}
+        <br>
+        <span class='bio'>${bio}</span></h2>
+    </div>`
     }
 }
+
+
 
 export default Dog
--- index.js
+++ index.js
@@ -1,2 +1,41 @@
-import dogsData from "./data.js";
-import Dog from "./Dog.js";
+import Dog from './Dog.js'
+
+const rex = {
+    name: "Rex",
+    avatar: "images/diegoT.jpg",
+    age: 25,
+    bio: "Art. Literature. Natural wine. Yoga.",
+    hasBeenSwiped: false,
+    hasBeenLiked: false
+}
+
+const bella = {
+    name: "Bella",
+    avatar: "images/georgeT.jpg",
+    age: 43,
+    bio: "Yup, that's my owner. U can meet him if you want",
+    hasBeenSwiped: false,
+    hasBeenLiked: false
+}
+
+const teddy = {
+    name: "Teddy",
+    avatar: "images/tacoT.jpg",
+    age: 30,
+    bio: "How you doin?",
+    hasBeenSwiped: false,
+    hasBeenLiked: false
+}
+
+
+
+
+/*--------------Constructor-----------*/
+const teddyTheDog = new Dog(bella)
+teddyTheDog.getDogHtml()
+
+
+/*-------------Constructor------------*/
+
+
+
