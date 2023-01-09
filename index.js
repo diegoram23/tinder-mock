4d4abdd4049d64236c6aacbdbf889abbcc12da6c
diegoram23 <diego.pta.oc@gmail.com>
2023-01-09 10:24:16 -0800

    Refactored code

--- index.js
+++ index.js
@@ -1,50 +1,6 @@
 import Profile from './Profile.js'
 import profileData from './data.js'
-
-
-
-
-let intro = document.querySelector('.intro')
-let logo = document.querySelector('.logo-header')
-let logoSpan = document.querySelectorAll('.logo')
-
-window.addEventListener('DOMContentLoaded', () => {
-
-    setTimeout(() => {
-
-        logoSpan.forEach((span, idx) => {
-            setTimeout(() => {
-                span.classList.add('active');
-            }, (idx + 1) * 400);
-        });
-
-        setTimeout(() => {
-            logoSpan.forEach((span, idx) => {
-
-                setTimeout(() => {
-                    span.classList.remove('active')
-                    span.classList.add('fade')
-                }, (idx + 1) * 50);
-            })
-        }, 2000);
-
-        setTimeout(() => {
-            intro.style.top = '-100vh'
-        }, 2300);
-    });
-})
-
-
-
-
-
-
-
-
-
-
-
-
+import animation from './utils.js'
 
 //Starts array of profiles at first index
 let profileIndex = 0;
